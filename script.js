4b0af00d2c535a41c94f97772e514d3ad9496637
BeanyTheCoder <alexanderafoko@gmail.com>
2023-08-08 20:07:11 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// let inputs = document.querySelectorAll(".input input");
+
+// let yearReporter = document.querySelector("#yearReporter");
+// let monthReporter = document.querySelector("#monthReporter");
+// let dayReporter = document.querySelector("#dayReporter");
+
+// let submitBtn = document.querySelector("#submitBtn");
+
+// function validate(year, month, day) {
+
+// }
+
+
+// submitBtn.addEventListener('click', () => {
+//   let values = []
+//   inputs.forEach(input => {
+//     values.push(input.value)
+//   })
+
+//   console.log(values)
+// })
